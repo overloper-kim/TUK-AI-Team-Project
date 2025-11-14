0a2721d6c2ef9a7369d481c5223b90ba23a354d2
overloper <overloper0225@gmail.com>
2025-11-14 20:57:32 +0900

    design: sidebar update

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,16 @@ function App(): React.ReactElement {
   return (
     <main className="bg-[#404040]">
       <SideBar/>
+      <div className="flex flex-row">
+        <section>
+          <p>시뮬레이션 환경</p>
+        </section>
+        <section>
 
+        </section>
+      </div>
     </main>
   )
 }
 
-export default App
+export default App;
