ffaa4876c70ddc8741e545d0a0dba4d50e951f80
overloper <overloper0225@gmail.com>
2025-12-08 23:24:14 +0900

    feat: add car lane

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import { useConfigureStore } from "./store/useConfigreStore";
 function App(): React.ReactElement {
 
   const lane = useConfigureStore((s) => s.lane);
+  // 기본 차량 위치를 중앙 차선으로 두도록 laneCount에 맞춰 계산
+  const carLaneCenter = (lane - 1) / 2;
 
   return (
     <main className="bg-[#404040] flex flex-row">
@@ -16,7 +18,7 @@ function App(): React.ReactElement {
       <div className="flex w-full flex-col">
         <Header/>
         <section className="size-full">
-          <AiDisplay carState={{x: 10, y: 20, stop: false}} laneCount={lane} />
+          <AiDisplay carState={{x: carLaneCenter, y: 0, stop: false}} laneCount={lane} />
         </section>
       </div>
     </main>
